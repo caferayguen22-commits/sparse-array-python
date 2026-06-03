d9f9a2cd8d065a09d54ce632aa79db14dbe0811b
caferayguen22-commits <cafer.ayguen22@gmail.com>
2026-06-03 23:03:31 +0200

    feat: implement __getitem__ for index retrieval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class SparseArray:
-    def __init__(selfself, initial_list):
+    def __init__(self, initial_list):
         # Wir speichern die virtuelle Gesamtlänge
         self._length = len(initial_list)
         # Hier landen nur die Werte, die NICHT Null sind
@@ -13,3 +13,12 @@ class SparseArray:
     def __len__(self):
         # Gibt die virtuelle Länge (inklusive der Nullen) zurück
         return self._length
+
+    # Werte abrufen mit sa[index]
+    def __getitem__(self, index):
+        # Wenn der Index außerhalb der erlaubten Länge liegt -> Fehler!
+        if index < 0 or index >= self._length:
+            raise IndexError("Index out of range")
+
+        # Hole den Wert aus dem Dict. Wenn er nicht existiert, gib 0 zurück
+        return self._data.get(index, 0)
